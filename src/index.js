3ec13aaa1312fb102b8c1b70a9e62c59a54c0ce0
gaddyya <gaddyya@gmail.com>
2018-09-27 23:48:15 +0300

    seventh task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let num = number;
+    let sumZero = 0;
+    let memoize = 0;
+
+    do{
+        memoize = Math.floor(num / 5);
+        num = memoize;
+        sumZero = memoize + sumZero;
+    }
+    while(memoize > 0)
+
+    return sumZero;
 }
